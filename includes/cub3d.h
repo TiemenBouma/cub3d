079d45b97de70ce64070b6f53b95e316106c98d7
tiemen <tiemen88@gmail.com>
2022-10-18 12:35:44 +0200

    Added MLX

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,4 +1,23 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+typedef struct s_cube
+{
+	int		res_x;
+	int		res_y;
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	char	*s;
 
+	char	**map;
+}	t_cube;
+
+typedef struct s_vars
+{
+	t_cube			*cude;
+	mlx_t			*mlx;
+}	t_vars;
+
+#endif
