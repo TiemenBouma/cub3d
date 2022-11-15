d61dff520ff3f0024e31a5de677f000ad47657b1
Sietse Vos <svos@f1r6s11.codam.nl>
2022-11-15 13:12:04 +0100

    made mlx_move_hook.c and mlx_rotate_hook.c norminetjes

--- src/mlx_move_hooks.c
+++ src/mlx_move_hooks.c
@@ -3,84 +3,106 @@
 
 void	hook_move_up(t_vars *vars, double moveSpeed)
 {
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	char	**map;
+
+	pos_x = vars->pov->pos_x;
+	pos_y = vars->pov->pos_y;
+	dir_x = vars->pov->dir_x;
+	dir_y = vars->pov->dir_y;
+	map = vars->cube->map;
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_W))
 	{
-		if(vars->cube->map[(int)(vars->pov->pos_y + 0.1)][(int)(vars->pov->pos_x + vars->pov->dir_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - 0.1)][(int)(vars->pov->pos_x + vars->pov->dir_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x + vars->pov->dir_x * moveSpeed + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x + vars->pov->dir_x * moveSpeed - 0.1)] != '1') 
-			vars->pov->pos_x += vars->pov->dir_x * moveSpeed;
-		if(vars->cube->map[(int)(vars->pov->pos_y + vars->pov->dir_y * moveSpeed + 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y + vars->pov->dir_y * moveSpeed - 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y + vars->pov->dir_y * moveSpeed)][(int)(vars->pov->pos_x + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y + vars->pov->dir_y * moveSpeed)][(int)(vars->pov->pos_x - 0.1)] != '1') 
-			vars->pov->pos_y += vars->pov->dir_y * moveSpeed;
-		else
-		{
-			printf("hit wall\n");
-		}
+		if (map[(int)(pos_y + 0.1)][(int)(pos_x + dir_x * moveSpeed)] != '1'
+			&& map[(int)(pos_y - 0.1)][(int)(pos_x + dir_x * moveSpeed)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x + dir_x * moveSpeed + 0.1)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x + dir_x * moveSpeed - 0.1)] != '1')
+			pos_x += dir_x * moveSpeed;
+		if (map[(int)(pos_y + dir_y * moveSpeed + 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y + dir_y * moveSpeed - 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y + dir_y * moveSpeed)][(int)(pos_x + 0.1)] != '1'
+			&& map[(int)(pos_y + dir_y * moveSpeed)][(int)(pos_x - 0.1)] != '1')
+			pos_y += dir_y * moveSpeed;
 	}
 }
 
 void	hook_move_down(t_vars *vars, double moveSpeed)
 {
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	char	**map;
+
+	pos_x = vars->pov->pos_x;
+	pos_y = vars->pov->pos_y;
+	dir_x = vars->pov->dir_x;
+	dir_y = vars->pov->dir_y;
+	map = vars->cube->map;
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_S))
 	{
-		if(vars->cube->map[(int)(vars->pov->pos_y + 0.1)][(int)(vars->pov->pos_x - vars->pov->dir_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - 0.1)][(int)(vars->pov->pos_x - vars->pov->dir_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x - vars->pov->dir_x * moveSpeed + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x - vars->pov->dir_x * moveSpeed - 0.1)] != '1') 
-			vars->pov->pos_x -= vars->pov->dir_x * moveSpeed;
-		if(vars->cube->map[(int)(vars->pov->pos_y - vars->pov->dir_y * moveSpeed + 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - vars->pov->dir_y * moveSpeed - 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - vars->pov->dir_y * moveSpeed)][(int)(vars->pov->pos_x + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - vars->pov->dir_y * moveSpeed)][(int)(vars->pov->pos_x - 0.1)] != '1') 
-			vars->pov->pos_y -= vars->pov->dir_y * moveSpeed;
-		else
-		{
-			printf("hit wall\n");
-		}
+		if (map[(int)(pos_y + 0.1)][(int)(pos_x - dir_x * moveSpeed)] != '1'
+			&& map[(int)(pos_y - 0.1)][(int)(pos_x - dir_x * moveSpeed)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x - dir_x * moveSpeed + 0.1)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x - dir_x * moveSpeed - 0.1)] != '1')
+			pos_x -= dir_x * moveSpeed;
+		if (map[(int)(pos_y - dir_y * moveSpeed + 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y - dir_y * moveSpeed - 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y - dir_y * moveSpeed)][(int)(pos_x + 0.1)] != '1'
+			&& map[(int)(pos_y - dir_y * moveSpeed)][(int)(pos_x - 0.1)] != '1')
+			pos_y -= dir_y * moveSpeed;
 	}
 }
 
-void	hook_move_left(t_vars *vars, double moveSpeed, double dir_perp_x, double dir_perp_y)
+void	hook_move_left(t_vars *vars, double moveSpeed,
+			double perpx, double perpy)
 {
+	double	pos_x;
+	double	pos_y;
+	char	**map;
+
+	pos_x = vars->pov->pos_x;
+	pos_y = vars->pov->pos_y;
+	map = vars->cube->map;
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_A))
 	{
-		if(vars->cube->map[(int)(vars->pov->pos_y + 0.1)][(int)(vars->pov->pos_x + dir_perp_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - 0.1)][(int)(vars->pov->pos_x + dir_perp_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x + dir_perp_x * moveSpeed + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x + dir_perp_x * moveSpeed - 0.1)] != '1') 
-			vars->pov->pos_x += dir_perp_x * moveSpeed;
-		if(vars->cube->map[(int)(vars->pov->pos_y + dir_perp_y * moveSpeed + 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y + dir_perp_y * moveSpeed - 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y + dir_perp_y * moveSpeed)][(int)(vars->pov->pos_x + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y + dir_perp_y * moveSpeed)][(int)(vars->pov->pos_x - 0.1)] != '1') 
-			vars->pov->pos_y += dir_perp_y * moveSpeed;
-		else
-		{
-			printf("hit wall\n");
-		}
+		if (map[(int)(pos_y + 0.1)][(int)(pos_x + perpx * moveSpeed)] != '1'
+			&& map[(int)(pos_y - 0.1)][(int)(pos_x + perpx * moveSpeed)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x + perpx * moveSpeed + 0.1)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x + perpx * moveSpeed - 0.1)] != '1')
+			pos_x += perpx * moveSpeed;
+		if (map[(int)(pos_y + perpy * moveSpeed + 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y + perpy * moveSpeed - 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y + perpy * moveSpeed)][(int)(pos_x + 0.1)] != '1'
+			&& map[(int)(pos_y + perpy * moveSpeed)][(int)(pos_x - 0.1)] != '1')
+			pos_y += perpy * moveSpeed;
 	}
 }
 
-void	hook_move_right(t_vars *vars, double moveSpeed, double dir_perp_x, double dir_perp_y)
+void	hook_move_right(t_vars *vars, double moveSpeed,
+			double perpx, double perpy)
 {
+	double	pos_x;
+	double	pos_y;
+	char	**map;
+
+	pos_x = vars->pov->pos_x;
+	pos_y = vars->pov->pos_y;
+	map = vars->cube->map;
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_D))
 	{
-		if(vars->cube->map[(int)(vars->pov->pos_y + 0.1)][(int)(vars->pov->pos_x - dir_perp_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - 0.1)][(int)(vars->pov->pos_x - dir_perp_x * moveSpeed)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x - dir_perp_x * moveSpeed + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y)][(int)(vars->pov->pos_x - dir_perp_x * moveSpeed - 0.1)] != '1') 
-			vars->pov->pos_x -= dir_perp_x * moveSpeed;
-		if(vars->cube->map[(int)(vars->pov->pos_y - dir_perp_y * moveSpeed + 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - dir_perp_y * moveSpeed - 0.1)][(int)(vars->pov->pos_x)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - dir_perp_y * moveSpeed)][(int)(vars->pov->pos_x + 0.1)] != '1'
-			&& vars->cube->map[(int)(vars->pov->pos_y - dir_perp_y * moveSpeed)][(int)(vars->pov->pos_x - 0.1)] != '1') 
-			vars->pov->pos_y -= dir_perp_y * moveSpeed;
-		else
-		{
-			printf("hit wall\n");
-		}
+		if (map[(int)(pos_y + 0.1)][(int)(pos_x - perpx * moveSpeed)] != '1'
+			&& map[(int)(pos_y - 0.1)][(int)(pos_x - perpx * moveSpeed)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x - perpx * moveSpeed + 0.1)] != '1'
+			&& map[(int)(pos_y)][(int)(pos_x - perpx * moveSpeed - 0.1)] != '1')
+			pos_x -= perpx * moveSpeed;
+		if (map[(int)(pos_y - perpy * moveSpeed + 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y - perpy * moveSpeed - 0.1)][(int)(pos_x)] != '1'
+			&& map[(int)(pos_y - perpy * moveSpeed)][(int)(pos_x + 0.1)] != '1'
+			&& map[(int)(pos_y - perpy * moveSpeed)][(int)(pos_x - 0.1)] != '1')
+			pos_y -= perpy * moveSpeed;
 	}
 }
--- src/mlx_rotate_hooks.c
+++ src/mlx_rotate_hooks.c
@@ -3,26 +3,40 @@
 
 void	hook_rotate_left(t_vars *vars, double rotSpeed)
 {
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = vars->pov->dir_x;
+	old_plane_x = vars->pov->plane_x;
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
 	{
-		double oldDirX = vars->pov->dir_x;
-		vars->pov->dir_x = vars->pov->dir_x * cos(rotSpeed) - vars->pov->dir_y * sin(rotSpeed);
-		vars->pov->dir_y = oldDirX * sin(rotSpeed) + vars->pov->dir_y * cos(rotSpeed);
-		double oldPlaneX = vars->pov->plane_x;
-		vars->pov->plane_x = vars->pov->plane_x * cos(rotSpeed) - vars->pov->plane_y * sin(rotSpeed);
-		vars->pov->plane_y = oldPlaneX * sin(rotSpeed) + vars->pov->plane_y * cos(rotSpeed);
+		vars->pov->dir_x = vars->pov->dir_x * cos(rotSpeed)
+			- vars->pov->dir_y * sin(rotSpeed);
+		vars->pov->dir_y = old_dir_x * sin(rotSpeed)
+			+ vars->pov->dir_y * cos(rotSpeed);
+		vars->pov->plane_x = vars->pov->plane_x * cos(rotSpeed)
+			- vars->pov->plane_y * sin(rotSpeed);
+		vars->pov->plane_y = old_plane_x * sin(rotSpeed)
+			+ vars->pov->plane_y * cos(rotSpeed);
 	}
 }
 
 void	hook_rotate_right(t_vars *vars, double rotSpeed)
 {
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = vars->pov->dir_x;
+	old_plane_x = vars->pov->plane_x;
 	if (mlx_is_key_down(vars->mlx, MLX_KEY_RIGHT))
 	{
-    double oldDirX = vars->pov->dir_x;
-    vars->pov->dir_x = vars->pov->dir_x * cos(-rotSpeed) - vars->pov->dir_y * sin(-rotSpeed);
-    vars->pov->dir_y = oldDirX * sin(-rotSpeed) + vars->pov->dir_y * cos(-rotSpeed);
-    double oldPlaneX = vars->pov->plane_x;
-    vars->pov->plane_x = vars->pov->plane_x * cos(-rotSpeed) - vars->pov->plane_y * sin(-rotSpeed);
-    vars->pov->plane_y = oldPlaneX * sin(-rotSpeed) + vars->pov->plane_y * cos(-rotSpeed);
+		vars->pov->dir_x = vars->pov->dir_x * cos(-rotSpeed)
+			- vars->pov->dir_y * sin(-rotSpeed);
+		vars->pov->dir_y = old_dir_x * sin(-rotSpeed)
+			+ vars->pov->dir_y * cos(-rotSpeed);
+		vars->pov->plane_x = vars->pov->plane_x * cos(-rotSpeed)
+			- vars->pov->plane_y * sin(-rotSpeed);
+		vars->pov->plane_y = old_plane_x * sin(-rotSpeed)
+			+ vars->pov->plane_y * cos(-rotSpeed);
 	}
 }
